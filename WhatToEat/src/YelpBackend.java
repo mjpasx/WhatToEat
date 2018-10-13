6c8f657692bf31010e37ae34dc5ce3f23ec9ea7a
Brendan Leech <Bleecher@brendans-mbp.middlebury.edu>
2018-10-13 10:51:41 -0400

    Initial commit of the backend

---
+++ WhatToEat/src/YelpBackend.java
@@ -0,0 +1,22 @@
+// Backend code to get reviews from a business name using Yelp dataset
+
+import java.io.*;
+import java.util.Scanner;
+
+public class YelpBackend
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the restaurant name: ");
+        String restName = scan.nextLine();
+        
+        scan.close();
+    }
+    /*
+    public static FindBusinessId(String restName)
+    {
+
+    }
+    */
+}
